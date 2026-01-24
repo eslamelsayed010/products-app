029549dfd19939c20d71a75208c38d5d0138a7b4
Eslam Elsayed <eslimelseyd@gmail.com>
2026-01-25 01:15:48 +0200

    Add mock API client and network service for unit testing

---
+++ ProductsAppTests/MockAPIClient.swift
@@ -0,0 +1,8 @@
+//
+//  MockAPIClient.swift
+//  ProductsAppTests
+//
+//  Created by Macos on 25/01/2026.
+//
+
+import Foundation
---
+++ ProductsAppTests/NetworkServiceTests.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkServiceTests.swift
+//  ProductsAppTests
+//
+//  Created by Macos on 25/01/2026.
+//
+
+import Foundation
