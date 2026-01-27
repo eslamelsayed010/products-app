256226691bb452d95c999174cc9dcf398179d823
Eslam Elsayed <eslimelseyd@gmail.com>
2026-01-27 23:49:15 +0200

    Create Custom SkeletonView as indicator while loading list data

---
+++ ProductsApp/Utils/CustomView/SkeletonView.swift
@@ -0,0 +1,20 @@
+//
+//  SkeletonView.swift
+//  ProductsApp
+//
+//  Created by Macos on 27/01/2026.
+//
+
+import UIKit
+
+class SkeletonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
