d4be2497b2d0dd465321716c77b53e052c04f98a
Eslam Elsayed <eslimelseyd@gmail.com>
2026-01-28 23:56:07 +0200

    Replace custom skeleton view with SkeletonView library in product cell

---
+++ ProductsApp/Views/ProductsView/Components/ProductsTopBarView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductsTopBarView.swift
+//  ProductsApp
+//
+//  Created by Macos on 28/01/2026.
+//
+
+import UIKit
+
+class ProductsTopBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
