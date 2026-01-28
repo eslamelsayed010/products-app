655f04ef83f690082a30a97c017ae7826bb22379
Eslam Elsayed <eslimelseyd@gmail.com>
2026-01-29 01:29:49 +0200

    Caching the products for no internet connection

---
+++ ProductsApp/Data/Cache/CacheManager.swift
@@ -0,0 +1,8 @@
+//
+//  CacheManager.swift
+//  ProductsApp
+//
+//  Created by Macos on 29/01/2026.
+//
+
+import Foundation
---
+++ ProductsApp/Utils/CustomView/OfflineBannerView.swift
@@ -0,0 +1,20 @@
+//
+//  OfflineBannerView.swift
+//  ProductsApp
+//
+//  Created by Macos on 29/01/2026.
+//
+
+import UIKit
+
+class OfflineBannerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
