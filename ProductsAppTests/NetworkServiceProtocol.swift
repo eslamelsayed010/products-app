37284abccdb406d1b4a718e5b852a5d90dcbe8db
Eslam Elsayed <eslimelseyd@gmail.com>
2026-01-28 23:28:55 +0200

    Add mock network service and products view model for unit testing

---
+++ ProductsAppTests/NetworkServiceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ProductsAppTests
+//
+//  Created by Macos on 28/01/2026.
+//
+
+import Foundation
---
+++ ProductsAppTests/ProductsListViewModelTests.swift
@@ -0,0 +1,8 @@
+//
+//  ProductsListViewModelTests.swift
+//  ProductsAppTests
+//
+//  Created by Macos on 28/01/2026.
+//
+
+import Foundation
